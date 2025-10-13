45e58245adf6c65bff71c13a0e1a778d9eb75d9d
zeinebibrahim <zeineb.ibrahim01@outlook.com>
2025-10-13 14:34:29 +0200

    bugfixes

--- VRconceptUnity/Assets/Unity UI Samples/Scripts/TiltWindow.cs
+++ VRconceptUnity/Assets/Unity UI Samples/Scripts/TiltWindow.cs
@@ -22,7 +22,7 @@ public class TiltWindow : MonoBehaviour
 		float halfHeight = Screen.height * 0.5f;
 		float x = Mathf.Clamp((pos.x - halfWidth) / halfWidth, -1f, 1f);
 		float y = Mathf.Clamp((pos.y - halfHeight) / halfHeight, -1f, 1f);
-		mRot = Vector2.Lerp(mRot, new Vector2(x, y), Time.deltaTime * 5f);
+		mRot = Vector2.Lerp(mRot, new Vector2(x, y), UnityEngine.Time.deltaTime * 5f);
 
 		mTrans.localRotation = mStart * Quaternion.Euler(-mRot.y * range.y, mRot.x * range.x, 0f);
 	}
