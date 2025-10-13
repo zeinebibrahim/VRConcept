cb5c548baafee1928b17b216c6b382e8cbd7fd82
LeonBijker <0351849@student.rocvantwente.nl>
2025-10-13 11:43:57 +0200

    Add initial Time MonoBehaviour script
    Introduced a new Time.cs script with a serialized Text field and basic Start and Update methods. This sets up the structure for future time-related functionality in the Unity project.

---
+++ VRconceptUnity/Assets/Scripts/Leon/Time.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Time : MonoBehaviour
+{
+    [SerializeField] Text TimeText;
+    private float _time;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
