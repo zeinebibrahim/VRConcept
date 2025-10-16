c00f8de5db75c1eac4cbb9a097e9a463896675ee
SimonBoersma <0354326@student.rocvantwente.nl>
2025-10-16 14:41:22 +0200

    Paint Splatter gefixed
    Paint Splatter gefixed door decals te gebruiken. Heb nieuw object en material gemaakt

--- VRconceptUnity/Assets/Scripts/Simon/PaintSplatter.cs
+++ VRconceptUnity/Assets/Scripts/Simon/PaintSplatter.cs
@@ -1,40 +1,32 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class PaintSplatter : MonoBehaviour
 {
-    [SerializeField] GameObject paintSplatterPrefab; // prefab van de verfspat
-
+    [SerializeField] GameObject decalPrefab;
 
     void OnCollisionEnter(Collision collision)
     {
-        Debug.Log(collision.gameObject);
-
-        // Spawn splatter
         ContactPoint contact = collision.contacts[0];
         Quaternion rot = Quaternion.LookRotation(-contact.normal, Vector3.up);
-
-        // Hier krijgt die een kleine offset zodat die niet IN de muur clipped
         float extraOffset = Random.Range(0.005f, 0.015f);
         Vector3 pos = contact.point + contact.normal * extraOffset;
 
+        GameObject decal = Instantiate(decalPrefab, pos, rot);
 
-        GameObject splat = Instantiate(paintSplatterPrefab, pos, rot);
-
-        // Random scale
-        float randomScale = Random.Range(0.5f, 1.5f);
-        splat.transform.localScale *= randomScale;
-
-        // Random draai rond de Z-as
-        splat.transform.Rotate(Vector3.forward, Random.Range(0f, 360f));
-
-        // Random kleur
+        //Kleur instellen
         Color randomColor = new Color(Random.value, Random.value, Random.value);
+        DecalProjector projector = decal.GetComponent<DecalProjector>();
+        Material matInstance = new Material(projector.material);
+        matInstance.SetColor("_SpatColor", randomColor);
+        projector.material = matInstance;
 
-        // Pas de kleur toe op de Renderer
-        Renderer r = splat.GetComponent<Renderer>();
-        if (r != null)
-        {
-            r.material.color = randomColor;
-        }
+        //Schaal en rotatie
+        float randomScale = Random.Range(0.5f, 1.5f);
+        decal.transform.localScale *= randomScale;
+        decal.transform.Rotate(Vector3.forward, Random.Range(0f, 360f), Space.Self);
+
+        //Zet als child van geraakt object
+        decal.transform.SetParent(collision.transform, true);
     }
 }
